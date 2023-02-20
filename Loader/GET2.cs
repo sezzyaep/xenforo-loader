3cd525f7af8171760fbe8b7c63761a4e1205059a
Berke <hackertr44@gmail.com>
2023-02-20 15:56:58 +0300

    Updated.

---
+++ Loader/GET2.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Text;
+
+namespace fuckhook
+{
+    internal class GET2
+    {
+        public static string Download(string adress)
+        {
+            string result;
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                result = wc.DownloadString(adress);
+            }
+            return result;
+        }
+    }
+}
---
+++ Loader/loading.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace fuckhook
+{
+    partial class loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loading));
+            this.guna2CircleProgressBar1 = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.SuspendLayout();
+            // 
+            // guna2CircleProgressBar1
+            // 
+            this.guna2CircleProgressBar1.Animated = true;
+            this.guna2CircleProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CircleProgressBar1.FillThickness = 3;
+            this.guna2CircleProgressBar1.Location = new System.Drawing.Point(12, 12);
+            this.guna2CircleProgressBar1.Name = "guna2CircleProgressBar1";
+            this.guna2CircleProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(176)))), ((int)(((byte)(242)))));
+            this.guna2CircleProgressBar1.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(176)))), ((int)(((byte)(242)))));
+            this.guna2CircleProgressBar1.ProgressThickness = 3;
+            this.guna2CircleProgressBar1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleProgressBar1.ShadowDecoration.Parent = this.guna2CircleProgressBar1;
+            this.guna2CircleProgressBar1.Size = new System.Drawing.Size(35, 35);
+            this.guna2CircleProgressBar1.TabIndex = 0;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Vermin Vibes V", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel1.ForeColor = System.Drawing.Color.White;
+            this.gunaLabel1.Location = new System.Drawing.Point(53, 19);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(77, 16);
+            this.gunaLabel1.TabIndex = 13;
+            this.gunaLabel1.Text = "Loading";
+            // 
+            // loading
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.ClientSize = new System.Drawing.Size(284, 102);
+            this.Controls.Add(this.gunaLabel1);
+            this.Controls.Add(this.guna2CircleProgressBar1);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "loading";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.loading_Load);
+            this.EnabledChanged += new System.EventHandler(this.loading_EnabledChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private Guna.UI2.WinForms.Guna2CircleProgressBar guna2CircleProgressBar1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+    }
+}
